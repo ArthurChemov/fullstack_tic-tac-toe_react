9a2771f55074482ef130357aa8c021967e94805b
Artur Chemov <arthur.chemov@gmail.com>
2023-02-13 13:03:22 +0200

    Vercel config

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 
-app.get('/', function(req, res) {
-  res.render('./routes/index');
-});
+// app.get('/', function(req, res) {
+//   res.render('./routes/index');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
